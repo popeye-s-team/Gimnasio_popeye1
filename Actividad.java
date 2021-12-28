c06185727f33e84495556bd25ebedf73e6726d81
marina.yague <marina.yague@edu.uah.es>
2021-12-28 12:21:59 +0100

    Co-authored-by: belss01 <belss01@users.noreply.github.com> Co-authored-by: sergio_onita <sergio-onita@users.noreply.github.com> Co-authored-by: Paula1710 <Paula1710@users.noreply.github.com> Co-authored-by: ccercosh <ccercosh@users.noreply.github.com>

---
+++ Actividad.java
@@ -0,0 +1,9 @@
+import java.util.UUID;
+
+public class Actividad {
+    private UUID id;
+    private String tipo;
+    private String horario;
+    private int aforo_max;
+    private String sala
+}
--- servicios_adicionales.java
+++ servicios_adicionales.java
@@ -3,3 +3,4 @@ import java.util.UUID;
 public class Servicios_adicionales{
     private UUID id;
     private String nombre;
+}
