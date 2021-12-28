238f0422f5b5f38feeb15f56300ae0d77848ce9b
marina.yague <marina.yague@edu.uah.es>
2021-12-28 12:39:37 +0100

    f

--- socio.java
+++ socio.java
@@ -7,11 +7,21 @@ public class Socio {
     private String mail;
     private String n_cuenta_bancaria;
     private UUID id;
+    private boolean alta;
 
     public Socio (String nombre, String telefono, String mail, String n_cuenta_bancaria, UUID id) {
         this.nombre=nombre;
         this.telefono=telefono;
         this.n_cuenta_bancaria=n_cuenta_bancaria;
         this.id=id;
+        this.alta = true;
+    }
+
+    public void darAlta(){
+        this.alta = true;
+    }
+    
+    public void darBaja(){
+        this.alta = false;
     }
 }
