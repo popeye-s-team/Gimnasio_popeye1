175b0ff882ef35d3e2382366a6628039b52fce77
marina.yague <marina.yague@edu.uah.es>
2021-12-28 12:31:30 +0100

    fhfhf

--- Monitor.java
+++ Monitor.java
@@ -5,4 +5,4 @@ public class Monitor extends Empleado{
         public Monitor (String dni, String nombre, Date fecha_nacimiento, float salario, String telefono, UUID id, String tipo_empleado, String usuario, String contrasena){
                 super(dni,nombre,fecha_nacimiento,salario,telefono,id,tipo_empleado,usuario,contrasena);    
         }
-} }
+} 
