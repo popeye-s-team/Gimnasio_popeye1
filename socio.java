4af052d74c04e3e2cbdea816aafb5980ae1df58d
marina.yague <marina.yague@edu.uah.es>
2021-12-28 12:17:03 +0100

    gd

--- socio.java
+++ socio.java
@@ -1,13 +1,14 @@
 import java.util.UUID;
 
 public class Socio{
+    
     private String nombre;
     private String telefono;
     private String mail;
     private String n_cuenta_bancaria;
     private UUID id;
 
-    public Socio(String nombre, String telefono, String mail, String n_cuenta_bancaria UUID id){
+    public Socio(String nombre, String telefono, String mail, String n_cuenta_bancaria, UUID id){
         this.nombre=nombre;
         this.telefono=telefono;
         this.n_cuenta_bancaria=n_cuenta_bancaria;
