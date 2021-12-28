a3c357be163fd153e75a6bb58e906faa3d7ec290
marina.yague <marina.yague@edu.uah.es>
2021-12-28 12:37:47 +0100

    g

--- Monitor.java
+++ Monitor.java
@@ -3,6 +3,6 @@ import java.util.UUID;
 
 public class Monitor extends Empleado{   
         public Monitor (String dni, String nombre, Date fecha_nacimiento, float salario, String telefono, UUID id, String tipo_empleado, String usuario, String contrasena){
-                super(dni,nombre,fecha_nacimiento,salario,telefono,id,tipo_empleado,usuario,contrasena);    
-        }
-} 
+                super(dni,nombre,fecha_nacimiento,salario,telefono,id,tipo_empleado,usuario,contrasena);
+        } 
+}
